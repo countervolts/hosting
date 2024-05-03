cbcee0f483d7b9e0c92d6a43b676de41e32dc0d9
ayo <93222141+countervolts@users.noreply.github.com>
2024-05-03 13:43:30 -0400

    hi

--- realscript.js
+++ realscript.js
@@ -2,39 +2,38 @@ const target = document.querySelector('.big-text');
 const text = target.textContent;
 const possibleLower = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const possibleUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-target.textContent = '';
 
 for (let i = 0; i < text.length; i++) {
     const span = document.createElement('span');
+    span.textContent = text[i];
     target.appendChild(span);
 
-    setTimeout(() => {
-        let randomString = '';
-        if (text[i] !== ' ') {
-            for (let j = 0; j < 10; j++) {
-                setTimeout(() => {
-                    if (i === 0 || text[i - 1] === ' ') {
-                        randomString = possibleUpper.charAt(Math.floor(Math.random() * possibleUpper.length));
-                    } else {
-                        randomString = possibleLower.charAt(Math.floor(Math.random() * possibleLower.length));
-                    }
-                    span.textContent = randomString;
-                }, j * 50);
-            }
-        }
+    if (text[i] !== ' ') {
+        let intervalId;
+        const shuffle = () => {
+            let randomString = '';
+            const possible = text[i] === text[i].toUpperCase() ? possibleUpper : possibleLower;
+            const possibleWithoutCurrent = possible.replace(text[i], '');
+            randomString = possibleWithoutCurrent.charAt(Math.floor(Math.random() * possibleWithoutCurrent.length));
+            span.textContent = randomString;
+        };
+
         setTimeout(() => {
+            for (let j = 0; j < 10; j++) {
+                setTimeout(shuffle, j * 50);
+            }
+            setTimeout(() => {
+                span.textContent = text[i];
+            }, 500);
+        }, i * 500);
+
+        span.addEventListener('mouseover', function() {
+            intervalId = setInterval(shuffle, 50);
+        });
+
+        span.addEventListener('mouseout', function() {
+            clearInterval(intervalId);
             span.textContent = text[i];
-        }, 500);
-    }, i * 500);
-
-    span.addEventListener('mouseover', function() {
-        const randomString = text[i] === text[i].toUpperCase() ?
-            possibleUpper.charAt(Math.floor(Math.random() * possibleUpper.length)) :
-            possibleLower.charAt(Math.floor(Math.random() * possibleLower.length));
-        span.textContent = randomString;
-    });
-
-    span.addEventListener('mouseout', function() {
-        span.textContent = text[i];
-    });
+        });
+    }
 }
